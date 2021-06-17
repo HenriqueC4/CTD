ab0b7fc7d40b830bf3b75a33a71902643a279510
Henrique César <contatohenriquec@gmail.com>
2021-06-17 12:37:14 -0300

    Envio de exercício transformando funções

---
+++ Programacao Imperativa/Módulo 2/Funcões Avancadas/transformando-funcoes.js
@@ -0,0 +1,26 @@
+// Transforme as seguintes funções em arrow functions
+
+// function print(mensagem)
+// {
+//     console.log(mensagem);
+// }
+
+// print("Olá, bom dia!");
+
+// function soma(n1,n2)
+// {
+//     return n1 + n2;
+// }
+
+// console.log(soma(10, 10));
+
+let print = mensagem => console.log(mensagem);
+
+print("Olá, bom dia!");
+
+let soma = (n1, n2) =>
+{
+     return n1 + n2;
+}
+
+console.log(soma(10, 10));
