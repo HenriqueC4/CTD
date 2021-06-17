ee4075e46e0d371d4cc3bb8d6e322309487d27c6
Henrique César <contatohenriquec@gmail.com>
2021-06-17 12:19:15 -0300

    Microdesafios da aula síncrona

---
+++ Programacao Imperativa/Aula 6 - Loops/microdesafios_loops.js
@@ -0,0 +1,69 @@
+//Percorra o array abaixo e exiba seus elementos
+const pontos = [12, 34, 56, 34, 5, 8, 76, 4, 1, 90];
+
+// for (let i = 0; i < pontos.length; i++)
+// {
+//     console.log(pontos[i]);
+// }
+
+// Crie um array com valores diversos, percorra-o, altere e mostre seus elementos
+
+var diverso = [25, 'Pedro', true, 198, 'Nhoque', false, 09, 'Mitocôndria', 908, 'Cálice'];
+
+// for (let i = 0; i < diverso.length; i++)
+// {
+//     diverso[i] = pontos[i]
+//     console.log(diverso[i]);
+// }
+
+//Crie outro array, percorra-o, altere, salve e exiba seus elementos
+
+var variado = [25, 'Carlito', true, 12, 'Nhoque', true, 09, 'Rabisco', 734, 'Carimbo Miguel'];
+
+// function salvar ()
+// {
+//     for (let i = 0; i < variado.length; i++)
+//     {
+//         diverso[i] = variado[i]
+//     }
+// }
+
+// function exibir()
+// {
+//     for (let i = 0; i < diverso.length; i++)
+//     {
+//         console.log(diverso[i]);
+//     }
+// }
+
+// salvar();
+// exibir();
+
+//Crie um último array, percorra-o, selecione os elementos, salve e mostre-os
+
+var numeros = [249, 452, 1341, 0925, 5835, 599, 94, 2429, 831, 121];
+
+// for (let i = 0; i < numeros.length; i++)
+// {
+//     variado[i] = numeros[i]
+//     console.log(variado[i]);
+// }
+
+//Escrever um programa que imprime 25 termos da série 11 - 22 - 33 - 44, etc.
+
+// x = 11;
+// for (i = 1; i <= 25; i++)
+// {
+//     console.log(x);
+//     x +=11
+// }
+
+//Mostrar os múltiplos de 8 até o valor 500. A tela deve mostrar 8 -16 -24
+
+
+for (i = 0; i < ; i++)
+{
+    
+}
+
+
