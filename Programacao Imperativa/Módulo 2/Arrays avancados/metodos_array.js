da7d7d32ca6f2bf3cd68eb8f5fbb63aabadeb043
Henrique César <contatohenriquec@gmail.com>
2021-07-05 19:29:31 -0300

    envio de atividade assincrona

--- Programacao Imperativa/Módulo 2/Arrays avancados/metodos_array.js
+++ Programacao Imperativa/Módulo 2/Arrays avancados/metodos_array.js
@@ -1,35 +1,35 @@
 // 1
 
-// let numeros = [2, 6, 8, 14, 16];
+let numeros = [2, 6, 8, 14, 16];
 
-// let impar = numeros.map(function(num)
-//     {
-//         return num + 1;
-//     });
+let impar = numeros.map(function(num)
+    {
+        return num + 1;
+    });
 
-// console.log(impar);
+console.log(impar);
 
 // 2
 
-// let nomes = ['Carlos', 'Maria', 'Ednaldo', 'Maria', 'Fausto'];
+let nomes = ['Carlos', 'Maria', 'Ednaldo', 'Maria', 'Fausto'];
 
-// let marias = nomes.filter(function(maria)
-//     {
-//         return maria == 'Maria'
-//     });
+let marias = nomes.filter(function(maria)
+    {
+        return maria == 'Maria'
+    });
 
-// console.log(marias);
+console.log(marias);
 
 // 3
 
-// let numeros = [1, 3, 5, 7, 9, 12, 15];
+let numeros = [1, 3, 5, 7, 9, 12, 15];
 
-// let format = numeros.reduce(function(a, b)
-//     {
-//         return a+'-'+b;
-//     });
+let format = numeros.reduce(function(a, b)
+    {
+        return a+'-'+b;
+    });
 
-// console.log(format);
+console.log(format);
 
 // 4
 
